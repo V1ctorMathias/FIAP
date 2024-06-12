b73c940ea8a25f5d4044f908bd8dc655ed9bf912
Victor Mathias <victormpporto@gmail.com>
2024-06-11 22:56:12 -0300

    adicionado novas alterações

--- 2024/F7.1/script.js
+++ 2024/F7.1/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            const details = button.nextElementSibling;
+            const details = button.previousElementSibling;
             if (details.style.display === 'none' || details.style.display === '') {
                 details.style.display = 'block';
                 button.textContent = 'Ver menos';

077da7713840bd799c5040a415eec99232d2db73
Victor Mathias <victormpporto@gmail.com>
2025-11-24 21:53:36 -0300

    Corrigida responsividade - HybridFlow

---
+++ ano3/globalSolution/hybridFlow/teste/bin/Release/net10.0/publish/wwwroot/js/index.js
@@ -0,0 +1,12 @@
+﻿console.log("Hello, World!");
+
+form = document.querySelector("form")
+
+botaoEnviar = document.querySelector("#enviar");
+
+//botaoEnviar.location.href = ""
+
+botaoEnviar.addEventListener('click', e => {
+
+    //e.preventDefault();
+});

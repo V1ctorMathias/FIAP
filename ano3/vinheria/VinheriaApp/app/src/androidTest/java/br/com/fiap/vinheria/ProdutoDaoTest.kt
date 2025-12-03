fc777d091a0d1c66ddf9f570a90478ffe05b7c78
Iago <iagobortolo@gmail.com>
2025-12-03 17:52:41 -0300

    inclusão do debug (println)

--- ano3/vinheria/VinheriaApp/app/src/androidTest/java/br/com/fiap/vinheria/ProdutoDaoTest.kt
+++ ano3/vinheria/VinheriaApp/app/src/androidTest/java/br/com/fiap/vinheria/ProdutoDaoTest.kt
@@ -8,12 +8,12 @@ import com.vinheriaagnello.data.ProdutoDao
 import com.vinheriaagnello.data.VinheriaDatabase
 import kotlinx.coroutines.runBlocking
 import org.junit.After
-import org.junit.Assert.assertEquals // Usar apenas o Assert do JUnit
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith // Import para o @RunWith
+import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class) // Anotação necessária para testes de instrumentação
+@RunWith(AndroidJUnit4::class)
 class ProdutoDaoTest {
 
     private lateinit var db: VinheriaDatabase
@@ -24,7 +24,7 @@ class ProdutoDaoTest {
         db = Room.inMemoryDatabaseBuilder(
             ApplicationProvider.getApplicationContext(),
             VinheriaDatabase::class.java
-        ).allowMainThreadQueries().build() // .allowMainThreadQueries() é útil para testes simples
+        ).allowMainThreadQueries().build()
         dao = db.produtoDao()
     }
 
@@ -45,6 +45,8 @@ class ProdutoDaoTest {
         dao.inserir(produto)
 
         val produtos = dao.listarTodos()
+
+        println("Produtos listados: $produtos")
 
         assertEquals(1, produtos.size)
         assertEquals("Vinho Tinto", produtos[0].nome)
